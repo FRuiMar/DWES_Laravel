ff185791ba1459fd630e3a622acbc30db13f1274
Fran Ruiz Martin <145322849+FRuiMar@users.noreply.github.com>
2025-01-30 10:30:44 +0100

    ACTUALIZO

--- Apuntes/explicacion/vite.config.js
+++ Apuntes/explicacion/vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+	plugins: [
+    	laravel({
+        	input: ["resources/css/app.css", "resources/js/app.js"],
+        	refresh: true,
+    	}),
+	],
+	build: {
+    	rollupOptions: {
+        	output: {
+            	manualChunks(id) {
+                	if (id.includes("node_modules")) {
+                    	return "vendor";
+                	}
+            	},
+        	},
+    	},
+	},
+	server: {
+    	origin: "http://localhost:5173",
+    	cors: true,
+	},
 });
